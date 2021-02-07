2211f46aa0d8b830d16b0c3dff1aefcda584278c
bharath sharma <bharath.sharma@dhullo.com>
2021-02-07 23:11:18 +0530

    dropdown

---
+++ src/StyledComponents/Styles.js
@@ -0,0 +1,14 @@
+import Styled from "styled-components";
+
+export const DropDown = Styled.div`
+   height:30rem;
+   width:20rem;
+ 
+   display:flex;
+   flex-direction:column;
+   position:absolute;
+   left:83%;
+   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+ background-color:white;
+   z-index:2000;
+`;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 import homeIcon from "../../assets/DesktopIconsSVG/DesktopIconsSVG/Icon material-home.svg"
 import msgIcon from "../../assets/DesktopIconsSVG/DesktopIconsSVG/Icon ionic-ios-chatboxes.svg"
 import notifIcon from "../../assets/DesktopIconsSVG/DesktopIconsSVG/Icon ionic-ios-notifications.svg"
 import giftIcon from "../../assets/DesktopIconsSVG/DesktopIconsSVG/Icon ionic-ios-gift.svg"
 import profilePic from "../../assets/DesktopIconsSVG/DesktopIconsSVG/Icon material-people.svg"
-
+import {DropDown} from '../../StyledComponents/Styles'
 
 import "./navbar.css";
 const Navbar = () => {
+  const [dropDown, setDropDown] = useState(false);
+
   return (
     <div className="nav-container">
       <nav>
@@ -17,21 +19,38 @@ const Navbar = () => {
         </div>
         <div className="spacer"></div>
         <div className="navlink">
-          <Link to="/"><img src = {homeIcon}></img></Link>
+          <Link to="/">
+            <img src={homeIcon}></img>
+          </Link>
         </div>
         <div className="navlink">
-          <Link to="/chat"><img src = {msgIcon}></img></Link>
+          <Link to="/chat">
+            <img src={msgIcon}></img>
+          </Link>
         </div>
         <div className="navlink">
-          <Link to="/merchandise"><img src = {giftIcon}></img></Link>
+          <Link to="/merchandise">
+            <img src={giftIcon}></img>
+          </Link>
         </div>
         <div className="navlink">
-          <Link to="/notification"><img src = {notifIcon}></img></Link>
+          <Link to="/notification">
+            <img src={notifIcon}></img>
+          </Link>
         </div>
         <div className="navlink">
-          <Link to="/login"><img src = {profilePic}></img></Link>
+          <Link  onClick={()=>setDropDown(!dropDown)}>
+           
+              <img src={profilePic}></img>
+           
+          </Link>
         </div>
       </nav>
+      {
+        dropDown ? 
+          <DropDown />
+          : null
+      }
     </div>
   );
 };
